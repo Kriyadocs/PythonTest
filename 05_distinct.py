d5409dd852f46c8030fcadfe8f7c7190b94d46a7
shinysu <shiny.suresh@gmail.com>
2022-05-01 22:33:03 +0530

    changed 05

---
+++ 05_distinct.py
@@ -0,0 +1,12 @@
+'''
+Consider an array/list of numbers, containing n distinct numbers in the range [0, n], 
+retrun the only number in the range that is missing from the array
+
+Sample Input:
+4 2 0 1
+
+Sample Output:
+3
+
+Here, n=4 and there are all the numbers between 0 and 4 except 3
+'''
--- 06_consonants.py
+++ 06_consonants.py
@@ -1,5 +1,6 @@
 '''
-Write a program that print the frequency of all consonants (not vowels) in a string
+Write a program that print the frequency of all consonants (not vowels) in a string. 
+The output should be printed in alphabetical order of letters
 
 Sample Input:
 hello
